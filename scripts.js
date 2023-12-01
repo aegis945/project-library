258831097387c20d0b86030a1cf651ec971da5be
aegis945 <artiom.lekincev@gmail.com>
2023-12-01 22:36:09 +0200

    added open and close(reset) form

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+const newBookBtn = document.querySelector('.add-book-btn');
+const newBookForm = document.querySelector('.modal-container');
+const cancelFormBtn = document.querySelector('.form-cancel-btn');
+const bookTitle = document.querySelector('#form-book-title');
+const bookAuthor = document.querySelector('#form-book-author');
+const bookLength = document.querySelector('#form-book-length');
+
 const myLibrary = [];
 
 function Book() {
@@ -6,4 +13,19 @@ function Book() {
 
 function addBookToLibrary() {
   // do stuff here
+}
+
+newBookBtn.addEventListener('click', function(){
+  newBookForm.style.display = 'block';
+})
+
+cancelFormBtn.addEventListener('click', function(){
+  newBookForm.style.display = 'none';
+  resetForm();
+})
+
+function resetForm() {
+  bookTitle.value = '';
+  bookAuthor.value = '';
+  bookLength.value = '';
 }
